9d60dd5ad27094b88437d259378cd43e36d98f17
DESKTOP-I3PECJ7\kawakupo <ryomaryoma123456@gmail.com>
2025-09-02 15:23:39 +0900

    PlayerController追加

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rbody; //PlayerについているRigidbodyを扱うための変数
+
+    float axisH; //入力の方向を記憶するための変数
+    public float speed = 5f;//プレイヤーのスピードを調整
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();//Playerについているコンポネント情報を取得
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Velocityの元となる値の取得(右なら1.0f、左なら-1.0f、何もなければ0)
+        axisH = Input.GetAxisRaw("Horizontal");
+
+
+
+    }
+    //1秒間に50回(50fps)繰り返すように制御しながら行う繰り返しメソッド
+    private void FixedUpdate()
+    {
+        //Velocityに値を代入
+        rbody.linearVelocity = new Vector2(axisH * speed, rbody.linearVelocity.y);
+    }
+}
