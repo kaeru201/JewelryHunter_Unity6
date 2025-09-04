ac71be826303cb32f68132a2d07078896d3c250d
DESKTOP-I3PECJ7\kawakupo <ryomaryoma123456@gmail.com>
2025-09-04 09:48:51 +0900

    issues用の画像ファイルを追加

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,40 +3,27 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [Header("プレイヤーの能力値")]
-    public float speed = 5f;//プレイヤーのスピードを調整
-    public float jumpPower = 9.0f;//ジャンプ力
+    public float speed = 3.0f; //プレイヤーのスピードを調整
+    public float jumpPower = 9.0f; //ジャンプ力
 
     [Header("地面判定の対象レイヤー")]
-    public LayerMask groundLayer;
+    public LayerMask groundLayer; //地面レイヤーを指名するための変数
 
-    Rigidbody2D rbody; //PlayerについているRigidbodyを扱うための変数
+    Rigidbody2D rbody; //PlayerについているRigidbody2Dを扱うための変数
     float axisH; //入力の方向を記憶するための変数
-    bool goJump = false;//ジャンプフラグ (ture:真on false:偽off)
-    bool onGround = false;//地面にいるかの判定
-
-
+    bool goJump = false; //ジャンプフラグ（true:真on、false:偽off)
+    bool onGround = false; //地面にいるかどうかの判定（地面にいる：true、地面にいない：false）
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rbody = GetComponent<Rigidbody2D>();//Playerについているコンポネント情報を取得
-
+        rbody = GetComponent<Rigidbody2D>(); //Playerについているコンポーネント情報を取得
     }
 
     // Update is called once per frame
     void Update()
     {
-        //地面判定をサークルキャストで行って、その結果を変数onGroundに代入
-        onGround = Physics2D.CircleCast(
-            transform.position, //発射位置=プレイヤーの位置
-            0.2f,
-            new Vector2(0, 1.0f),
-            0,
-            groundLayer
-            );
-
-
-        //Velocityの元となる値の取得(右なら1.0f、左なら-1.0f、何もなければ0)
+        //Velocityの元となる値の取得（右なら1.0f、左なら-1.0f、なにもなければ0)
         axisH = Input.GetAxisRaw("Horizontal");
 
         if (axisH > 0)
@@ -49,33 +36,46 @@ public class PlayerController : MonoBehaviour
             //左を向く
             transform.localScale = new Vector3(-1, 1, 1);
         }
-        //GetButtonDownメソッド→引数に指定したボタンが押されたらtureを返す、押されていなかったたらfalseを返す
+
+        //GetButtonDownメソッド→引数に指定したボタンが押されたらtrueを返す、押されていなければfalseを返す
         if (Input.GetButtonDown("Jump"))
         {
-            Jump();//jumpメソッドの発動
+            Jump(); //Jumpメソッドの発動
         }
 
-
     }
+
     //1秒間に50回(50fps)繰り返すように制御しながら行う繰り返しメソッド
-    private void FixedUpdate()
+    void FixedUpdate()
     {
+        //地面判定をサークルキャストで行って、その結果を変数onGroundに代入
+        onGround = Physics2D.CircleCast(
+            transform.position,   //発射位置＝プレイヤーの位置（基準点）
+            0.2f,                   //調査する円の半径
+            new Vector2(0, 1.0f),  //発射方向 ※下方向
+            0,                      //発射距離
+            groundLayer        //対象となるレイヤー情報 ※LayerMask
+            );
+
         //Velocityに値を代入
         rbody.linearVelocity = new Vector2(axisH * speed, rbody.linearVelocity.y);
 
+        //ジャンプフラグが立ったら
         if (goJump)
         {
+            //ジャンプさせる→プレイヤーを上に押し出す
             rbody.AddForce(new Vector2(0, jumpPower), ForceMode2D.Impulse);
-            goJump = false;
+            goJump = false; //フラグをOFFに戻す
         }
     }
 
+    //ジャンプボタンがおされた時に呼び出されるメソッド
     void Jump()
     {
         if (onGround)
         {
-            goJump = true;//ジャンプフラグをon
+            goJump = true; //ジャンプフラグをON
         }
-
     }
+
 }
